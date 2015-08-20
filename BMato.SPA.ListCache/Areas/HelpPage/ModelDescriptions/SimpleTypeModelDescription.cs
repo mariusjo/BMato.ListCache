83c27f519566fb5d158bdc57927a7b4de1ad44fc
Marius <mariusjohans@gmail.com>
2015-08-20 12:53:37 +0200

    renaming top folder

--- BMato.SPA.ListCache/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace BMato.SPA.ListCache.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- BMato.SPA.ListCache/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BMato.SPA.ListCache.MvcApplication" Language="C#" %>
